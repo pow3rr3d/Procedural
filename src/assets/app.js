1e0a5d5abf189ba0c15c82a25175b8434934f6f3
Pow3rr3d <adrien.messager.diaz@gmail.com>
2021-08-11 16:45:01 +0200

    Update Scss on toggled Menu

--- src/assets/app.js
+++ src/assets/app.js
@@ -49,13 +49,13 @@ var container = document.querySelector(".body")
 
 if (Cookies.get('menu')) {
     menu.classList.replace('notHidden', Cookies.get('menu'));
-    // menu.classList.contains('hidden') ? container.classList.add('col-12') : container.classList.remove('col-10');
+    menu.classList.contains('hidden') ? container.classList.add('col-11') : container.classList.remove('col-11');
 } else {
 
 }
 var toogleEvent = function () {
     menu.classList.contains('hidden') ? menu.classList.remove('hidden') : menu.classList.add('hidden');
-    // menu.classList.contains('hidden') ? container.classList.add('col-10') : container.classList.remove('col-12');
+    menu.classList.contains('hidden') ? container.classList.add('col-11') : container.classList.remove('col-11');
     if (menu.classList.contains('hidden')) {
         Cookies.set('menu', 'hidden')
     } else {

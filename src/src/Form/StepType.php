16f3511c8054a5f88573ccdca9578861ede433b3
Pow3rr3d <adrien.messager.diaz@gmail.com>
2021-08-11 17:36:01 +0200

    Working on ProcessType

--- src/src/Form/StepType.php
+++ src/src/Form/StepType.php
@@ -23,7 +23,7 @@ class StepType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => null,
+            'data_class' => Step::class,
         ]);
     }
 }

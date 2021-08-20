3317c238f9a677c95113f7191065d77ad567eecf
Adrien <adrien.messager.diaz@gmail.com>
2021-08-20 17:06:04 +0200

    Update package.json

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -26,7 +26,7 @@ Encore
 
     //JS
     .addEntry('app', './assets/app.js')
-    // .enableSassLoader()
+    .enableSassLoader()
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')

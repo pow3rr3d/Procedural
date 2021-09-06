9d8dcfbb616c0a50f1b35fc06465625b59cc2845
Adrien <adrien.messager.diaz@gmail.com>
2021-09-06 11:12:47 +0200

    Update Security controller

--- src/src/Controller/SecurityController.php
+++ src/src/Controller/SecurityController.php
@@ -40,6 +40,6 @@ class SecurityController extends AbstractController
      */
     public function index()
     {
-        return $this->redirectToRoute("app_login");
+        return $this->redirectToRoute("app_dashboard");
     }
 }
